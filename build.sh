5b4c52d8e7160cf569b2b17cee065afeab9359b8
MrWilliam-tu <turnerw@ill.fr>
2020-01-30 18:49:35 +0100

    Added configMap file to mount postgres config files to panosc postgres deployment, moved postgres password to secret file and delete panosc postgres dockerfile

--- build.sh
+++ build.sh
@@ -5,17 +5,9 @@ SERVICE_REPO_DIR=git_repos
 DOCKER_REGISTRY_HOST=panosc-docker.ill.fr
 
 
-# Build panosc-portal-postgres
-docker build -t panosc-portal-postgres panosc-portal-postgres-image
-
-# push panosc-portal-postgres to docker registry
-docker tag panosc-portal-postgres $DOCKER_REGISTRY_HOST/panosc-portal/postgres
-docker push $DOCKER_REGISTRY_HOST/panosc-portal/postgres
-
-
 # Create and clean git repos dir
 mkdir -p $SERVICE_REPO_DIR
-rm -rf "${$SERVICE_REPO_DIR:?}/"*
+rm -rf $SERVICE_REPO_DIR/*
 rm -rf $SERVICE_REPO_DIR/.* ||:
 
 
--- panosc-portal-demo/postgres-config-files/cloud-provider-kubernetes.sh
+++ panosc-portal-demo/postgres-config-files/cloud-provider-kubernetes.sh
@@ -5,9 +5,10 @@ POSTGRES="psql --username ${POSTGRES_USER}"
 
 echo "Creating users and databases"
 
+
 $POSTGRES <<EOSQL
 CREATE USER "cloud-provider-kubernetes" WITH CREATEDB PASSWORD '${CLOUD_PROVIDER_K8S_DATABASE_PASSWORD}';
 CREATE DATABASE "cloud-provider-kubernetes" OWNER "cloud-provider-kubernetes";
 EOSQL
 
-$POSTGRES -d cloud-provider-kubernetes -a -f /tmp/cloud-provider-kubernetes.sql
+$POSTGRES -d cloud-provider-kubernetes -a -f /tmp/cloud-provider-kubernetes.sql;
